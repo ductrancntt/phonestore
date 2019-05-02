8d25673a018f61dd3b3e7e601e0b7f42c4776f8a
Phạm Đức Cường <cuongphamduc1997@gmail.com>
2019-05-02 21:53:18 +0700

    Update mail

--- sendmail.php
+++ sendmail.php
@@ -1,39 +1,41 @@
-
-
-<?php     
+<?php
 include('SMTP.php');
-include("PHPMailer.php"); 
-use PHPMailer\PHPMailer\PHPMailer;
-function sendMail($to){
-	$nFrom = "no-reply";    //mail duoc gui tu dau, thuong de ten cong ty ban
-	$mFrom = 'phoneshopptit@gmail.com';  //dia chi email cua ban
-	$mPass = 'abc@@123';       //mat khau email cua ban
+include("PHPMailer.php");
 
-	$mail = new PHPMailer();
-	$body = 'Bạn đã đặt hàng thành công!';   // Noi dung email
-	$title = "Thông báo đặt hàng thành công";   //Tieu de gui mail
-	$mail->IsSMTP();             
-	$mail->CharSet  = "utf-8";
-	$mail->SMTPDebug  = 0;   // enables SMTP debug information (for testing)
-	$mail->SMTPAuth   = true;    // enable SMTP authentication
-	$mail->SMTPSecure = "ssl";   // sets the prefix to the servier
-	$mail->Host       = "smtp.gmail.com";    // sever gui mail.
-	$mail->Port       = 465;         // cong gui mail de nguyen
-	// xong phan cau hinh bat dau phan gui mail
-	$mail->Username   = $mFrom;  // khai bao dia chi email
-	$mail->Password   = $mPass;              // khai bao mat khau
-	$mail->SetFrom($mFrom, $nFrom);
-	$mail->AddReplyTo('phoneshopptit@gmail.com', 'phoneshop'); //khi nguoi dung phan hoi se duoc gui den email nay
-	$mail->Subject    = $title;// tieu de email 
-	$mail->MsgHTML($body);// noi dung chinh cua mail se nam o day.
-	$mail->AddAddress($to, $to);
-	// thuc thi lenh gui mail 
-	if(!$mail->Send()) {
-			return 1;
-	} else {
-			return 0;
-	}
+use PHPMailer\PHPMailer\PHPMailer;
+
+function sendMail($to)
+{
+    $nFrom = "Phone Shop PTIT";
+    $mFrom = 'phoneshopptit@gmail.com';
+    $mPass = 'abc@@123';
+
+    $mail = new PHPMailer();
+    $body = "Your order has been confirmed";
+    $title = "Successful order";
+    $mail->IsSMTP();
+    $mail->CharSet = "utf-8";
+    $mail->SMTPDebug = 0;
+    $mail->SMTPAuth = true;
+    $mail->SMTPSecure = "ssl";
+    $mail->Host = "smtp.gmail.com";
+    $mail->Port = 465;
+
+    $mail->Username = $mFrom;
+    $mail->Password = $mPass;
+    $mail->SetFrom($mFrom, $nFrom);
+    $mail->AddReplyTo('phoneshopptit@gmail.com', 'phoneshop');
+    $mail->Subject = $title;
+    $mail->MsgHTML($body);
+    $mail->AddAddress($to, $to);
+
+    if (!$mail->Send()) {
+        return 1;
+    } else {
+        return 0;
+    }
 }
+
 ?>
 
 
